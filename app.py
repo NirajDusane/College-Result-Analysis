e744042a57a8951370531ba7254a95924b922f18
NirajDusane <nirajdusane3064@gmail>
2025-12-28 12:18:24 +0530

    Added gunicorn for deployment

--- app.py
+++ app.py
@@ -266,6 +266,10 @@ def calculate_stats(student_list):
             
     return stats
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
 if __name__ == '__main__':
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.1', port=port)
     app.run(debug=True)
     
